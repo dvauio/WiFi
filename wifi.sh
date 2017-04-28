95d1fc710e5e36be9e87cf42d5ae939b0b60a4b5
dvauio <davidvaughan556@gmail.com>
2017-04-28 20:58:18 +0100

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,1 +1,35 @@
-test
+#!/bin/bash 
+
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt-get install haveged -y
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo echo /swapfile none swap sw 0 0 | tee -a /etc/fstab
+sudo echo 10 | sudo tee /proc/sys/vm/swappiness
+sudo echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf
+sudo sysctl vm.vfs_cache_pressure=50
+sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -I INPUT 1 -i lo -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 8081 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 8443 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 8880 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 8843 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 27117 -j ACCEPT
+sudo iptables -A INPUT -j DROP
+sudo apt-get install iptables-persistent -y
+sudo apt-get install fail2ban -y
+sudo echo deb http://www.ubnt.com/downloads/unifi/debian unifi5 ubiquiti | tee -a /etc/apt/sources.list
+sudo echo deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen | tee -a /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+sudo apt-get update
+sudo apt-get install unifi -y
+echo
+echo Congratulations UniFi is now installed.
+echo Access the controller at the following IP address using HTTPS on port 8443:
+/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
