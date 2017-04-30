48146ddecca7c5b6946abb7e0194bb6a346394e6
dvauio <davidvaughan556@gmail.com>
2017-04-30 19:16:12 +0100

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -15,9 +15,11 @@ sudo echo 10 | sudo tee /proc/sys/vm/swappiness
 sudo echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf
 sudo sysctl vm.vfs_cache_pressure=50
 
-echo IP rules 
+echo IP rules
 sudo apt install iptables-persistent -y
 sudo apt-get install fail2ban -y
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -I INPUT 1 -i lo -j ACCEPT
