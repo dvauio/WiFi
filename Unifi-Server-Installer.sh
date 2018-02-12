b7c3fccd87bf2757a84f046c09515fadd3e0b015
dvauio <davidvaughan556@gmail.com>
2018-02-12 14:31:30 +0000

    Update Unifi-Server-Installer.sh

--- Unifi-Server-Installer.sh
+++ Unifi-Server-Installer.sh
@@ -24,8 +24,6 @@ sudo apt-get install certbot -y
 echo Install haveged
 sudo apt-get install haveged -y
 
-echo UNMS  Installtion
-curl -fsSL https://raw.githubusercontent.com/Ubiquiti-App/UNMS/master/install.sh > /tmp/unms_install.sh && sudo bash /tmp/unms_install.sh --update
 
 echo Unifi Installtion
 sudo echo deb http://www.ubnt.com/downloads/unifi/debian unifi5 ubiquiti | tee -a /etc/apt/sources.list
